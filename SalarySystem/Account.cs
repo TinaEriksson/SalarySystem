884eb370ed953ccae6713fc7b4d991628740a928
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-11-10 14:26:59 +0100

    Started coding the program

--- SalarySystem/Account.cs
+++ SalarySystem/Account.cs
@@ -8,9 +8,17 @@ namespace SalarySystem
 {
     class Account
     {
+        static List<Account> users = new List<Account>();
+
         public string username { get; set; }
         public string password { get; set; }
         public string profession { get; set; }
         public int salary { get; set; }
+
+        public Account(string username, string password)
+        {
+            this.username = username;
+            this.password = password;
+        }
     }
 }
