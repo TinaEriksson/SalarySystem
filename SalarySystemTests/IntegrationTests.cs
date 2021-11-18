aeaec2a38a603e3be7d9b87b8d39e36336860e3c
Josefine <86josron@gafe.molndal.se>
2021-11-18 21:03:36 +0100

    Integationtest

---
+++ SalarySystemTests/IntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalarySystem;
+using SalarySystem.Tests;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalarySystemTests
+{
+    [TestClass()]
+    public class IntegrationTests
+    {
+        [TestMethod()]
+        public void IntegrationTest()
+        {
+            Employee employee = new()
+            {
+                username="Ulla",
+                password= "Ulla1234",
+                profession="Copiater",
+                salary= 10000,
+            };
+            AdminTests admin = new();
+            StartUpTests start = new();
+            EmployeeTests employeeTests = new();
+
+            admin.CreateEmployeeTest(employee.username, employee.password,
+                                    employee.profession, employee.salary);          
+            start.LogInTest(employee.username, employee.password);
+            start.FindUserTest(employee.username);          
+            employeeTests.DeleteMeTest(employee.username, employee.password);
+
+            A
+
+
+        }
+    }
+}
